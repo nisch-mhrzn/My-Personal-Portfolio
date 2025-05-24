ac639c88f6884990c263704f0616771f723c0964
Nischal Maharjan <nischal.maharjan1233@gmail.com>
2025-05-24 06:18:47 +0545

    Updated my experience and about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,28 +17,28 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Software Developer</h3>
+              <h3>Data Scientist</h3>
               <p>
-              I am a software developer with experience in building and designing softwares
+                I am an aspiring Data Scientist with experience in handling large datasets, performing data analysis, and creating insightful visualizations.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>Machine Learning Enthusiast</h3>
               <p>
-              I am a frontend developer with experience in building responsive
-              and optimized sites
+              Proficient in building, evaluating, and deploying machine learning models using Scikit-learn, TensorFlow, and other frameworks to solve real-world problems.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
-              <h3>Data Scientist</h3>
+              <h3>Data Analyst</h3>
               <p>
-              Experience in dealing with huge datasets
+                      Skilled in data cleaning, exploration, and visualization using tools like Pandas, NumPy, Matplotlib, and Seaborn to uncover actionable insights.
+
               </p>
             </div>
           </li>
